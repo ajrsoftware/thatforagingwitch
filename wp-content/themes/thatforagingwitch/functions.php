d44f62517fee19809add8177a0cfe3f3319a7280
Ashley Redman <ash.redman@outlook.com>
2023-06-24 12:00:39 +0100

    Added lumberjack

--- wp-content/themes/thatforagingwitch/functions.php
+++ wp-content/themes/thatforagingwitch/functions.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Lumberjack;
+
 define('THEME_NAME', 'thatforagingwitch');
 define('THEME_VERSION', '1.0');
 
-require_once('app/bootstrap.php');
-require_once('app/include/acf.php');
+require_once('path/to/composer/vendor/autoload.php');
+$app = require_once('bootstrap/app.php');
+
+$lumberjack = $app->make(Lumberjack::class);
+$lumberjack->bootstrap();
+
+require_once('app/routes.php');
 require_once('app/site.php');
+
